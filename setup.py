c9be735c68413185c576608c142c86880e823902
Hamdy Elbatal <36019461+hamdyelbatal@users.noreply.github.com>
2018-10-03 14:21:16 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "linode-python",
+    version = "1.2",
+    description = "Python bindings for Linode API",
+    author = "TJ Fontaine",
+    author_email = "tjfontaine@gmail.com",
+    url = "https://github.com/tjfontaine/linode-python",
+    packages = ['linode'],
+    extras_require = {
+        'requests': ["requests"],
+    },
+)
